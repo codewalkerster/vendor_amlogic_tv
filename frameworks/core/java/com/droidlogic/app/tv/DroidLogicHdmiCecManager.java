4f9ffbcdc92f611fa5336734c85a59634899a5cf
an.xi <an.xi@amlogic.com>
2023-12-14 14:01:33 +0800

    cec: don't do release job for launcher pip window [1/1]
    PD#SWPL-145094
    
    Problem:
    The release job for launcher pip window could cause
    a null pointer crash.
    
    Solution:
    Add null pointer check and don't do release job for
    launcher pip window.
    
    Verify:
    calla
    
    Change-Id: I87f69f204f965ba0c2bc00b1a55e60e18ec72877
    Signed-off-by: an.xi <an.xi@amlogic.com>

--- frameworks/core/java/com/droidlogic/app/tv/DroidLogicHdmiCecManager.java
+++ frameworks/core/java/com/droidlogic/app/tv/DroidLogicHdmiCecManager.java
@@ -286,6 +286,11 @@ public class DroidLogicHdmiCecManager {
         }
 
         if (isHdmiDeviceId(deviceId)) { // Hdmi device
+            if (isLauncherPipForeground()) {
+                // For android cts HdmiCecInvalidMessagesTest#cect_IgnoreBroadcastedFromSameSource etc.
+                Log.w(TAG, "onSetMain ignore routing control if droidlogic launcher pip is foreground");
+                return;
+            }
             if (isMain) {
                 TvInputInfo info = mTvInputManager.getTvInputInfo(inputId);
                 if (info == null) {
@@ -315,7 +320,9 @@ public class DroidLogicHdmiCecManager {
                 }
             } else {
                 // case 3: leave hdmi, try to device select 0
-                if (deviceId == mCurrentSelect.getDeviceId() && (sessionId != mCurrentSelect.getSessionId())) {
+                if (mCurrentSelect == null
+                    || (deviceId == mCurrentSelect.getDeviceId()
+                    && (sessionId != mCurrentSelect.getSessionId()))) {
                     // Same hdmi but different session id. In this case the sequence of setMain is not for sure.
                     return;
                 }
